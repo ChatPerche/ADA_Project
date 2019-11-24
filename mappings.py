a5d4acde1bb750711ab3667900c232908826f1e7
OnlineGardenGnome <OnlineGardenGnome@github.com>
2019-11-24 11:42:30 +0100

    Working LSTM

--- mappings.py
+++ mappings.py
@@ -1,6 +1,7 @@
 import os
 from glob import glob
 import pandas as pd
+import gc
 from data_processing import get_column_unique_values, load_dataframe
 
 def get_mapping(csv_files, col_rename, check_cols, columns):
@@ -18,3 +19,14 @@ def get_country_groups(csv):
     df = load_dataframe(csv)
     grouped = df.groupby('countrygroupcode')['countrycode'].agg(set).reset_index()
     return dict(grouped.values)
+
+def extract_element_to_item(df):
+    e_and_i = set(df.set_index(['areacode', 'year', 'itemcode']).unstack(level=-1) \
+                  .dropna(axis=1, how='all').columns.to_list())
+
+    e_to_i = {}
+    for e, i in e_and_i:
+        if e != 'Unnamed: 0':
+            e_to_i[e] = e_to_i[e] + [i] if e in e_to_i else [i]
+
+    return e_to_i
